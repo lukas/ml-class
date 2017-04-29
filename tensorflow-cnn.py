Fixing tensorflow argument call and final test accuracy calculation

tensorflow-cnn.py
@@ -56,7 +56,7 @@ b_fc2 = bias_variable([10])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -92,6 +92,6 @@ for i in range(1000):
 #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 test_accuracy = accuracy.eval(feed_dict={
-        x:batch_xs[1000:1100], y_: batch_ys[1000:1100], keep_prob: 1.0})
+        x:digits.data[1000:1100], y_: dense_target[1000:1100], keep_prob: 1.0})
 
-print("test accuracy %g"%(i, test_accuracy))
+print("test accuracy %g"%(test_accuracy))