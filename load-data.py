af40792158abb2661a44516aa01a91164cdbe04a
Lukas Biewald <lukeab@gmail.com>
2017-05-24 21:45:32 -0700

    minor changes

--- load-data.py
+++ load-data.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
 
-
 df = pd.read_csv('tweets.csv')
 target = df['is_there_an_emotion_directed_at_a_brand_or_product']
 text = df['tweet_text']
 
-print(target[0:5])
-print(text[0:5])
-
+#print(target[9])
+print(text[9])
