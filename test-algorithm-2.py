0a36d8c748be33f7868384c3257b8f9830d80f80
Lukas Biewald <lukeab@gmail.com>
2017-10-01 15:46:31 -0700

    Simplifying code

--- test-algorithm-2.py
+++ test-algorithm-2.py
@@ -28,8 +28,7 @@ counts = count_vect.transform(text)
 
 # Train with this data with a Naive Bayes classifier:
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import GradientBoostingClassifier
-nb = Gradient()
+nb = MultinomialNB()
 
 # (Tweets 0 to 5999 are used for training data)
 nb.fit(counts[0:6000], target[0:6000])
@@ -37,8 +36,6 @@ nb.fit(counts[0:6000], target[0:6000])
 # See what the classifier predicts for some new tweets:
 # (Tweets 6000 to 9091 are used for testing)
 predictions = nb.predict(counts[6000:9092])
+print(len(predictions))
 correct_predictions = sum(predictions == target[6000:9092])
-incorrect_predictions = (9092 - 6000) - correct_predictions
-print('# of correct predictions: ' + str(correct_predictions))
-print('# of incorrect predictions: ' + str(incorrect_predictions))
-print('Percent correct: ' + str(100.0 * correct_predictions / (correct_predictions + incorrect_predictions)))
+print('Percent correct: ', 100.0 * correct_predictions / 3092)
