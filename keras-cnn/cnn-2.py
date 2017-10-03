8de1ad1743c5702059881a828445d365234c1484
Lukas Biewald <lukeab@gmail.com>
2017-10-03 07:44:55 -0700

    bug fix

--- keras-cnn/cnn-2.py
+++ keras-cnn/cnn-2.py
@@ -39,4 +39,4 @@ model.compile(loss='categorical_crossentropy', optimizer='adam',
 model.fit(X_train, y_train, validation_data=(X_test, y_test),
                             callbacks=[WandbKerasCallback()], epochs=config.epochs)
 
-model.save("convnet.h5")
+
