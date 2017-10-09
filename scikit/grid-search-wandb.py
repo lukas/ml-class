358a789c6e1aec412e7903ff52b12716dbfd5e66
Lukas Biewald <lukeab@gmail.com>
2017-10-09 15:50:55 -0700

    Updating wandb settings

--- scikit/grid-search-wandb.py
+++ scikit/grid-search-wandb.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+import wandb
 
+run = wandb.init()
+config = run.config
+summary = run.summary
 
 df = pd.read_csv('tweets.csv')
 target = df['is_there_an_emotion_directed_at_a_brand_or_product']
@@ -21,23 +25,31 @@ p = Pipeline(steps=[('counts', CountVectorizer()),
 
 from sklearn.model_selection import GridSearchCV
 
-
+# these parameters define the different configurations we are going to try
+# in our grid search
 
 parameters = {
-    'counts__max_df': (0.5, 0.75,1.0),
-    'counts__min_df': (0,1,2),
+#    'counts__max_df': (0.5, 0.75,1.0),
+#    'counts__min_df': (0,1,2),
 #    'counts__token_pattern': ('(?u)\b\w\w+\b', '(?u)\b\w\w+\b'),
     'counts__lowercase' : (True, False),
     'counts__ngram_range': ((1,1), (1,2)),
 #    'feature_selection__k': (1000, 10000, 100000)
+    'multinomialnb__alpha': (0.5, 1)
     }
 
-grid_search = GridSearchCV(p, parameters, n_jobs=1, verbose=1, cv=10)
+# setup the grid search - increasing n_jobs will make more threads
+grid_search = GridSearchCV(p, parameters, n_jobs=1, verbose=2, cv=2)
 
+# do the actual grid search
 grid_search.fit(fixed_text, fixed_target)
 
+# output the results and the parameters that obtained those results
 print("Best score: %0.3f" % grid_search.best_score_)
 print("Best parameters set:")
 best_parameters = grid_search.best_estimator_.get_params()
 for param_name in sorted(parameters.keys()):
     print("\t%s: %r" % (param_name, best_parameters[param_name]))
+    config[param_name] = best_parameters[param_name]
+
+summary['mean_accuracy'] = grid_search.best_score_
