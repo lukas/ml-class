d70e3bfde8961f05dc52b9db79aca1fa93ca0683
Lukas Biewald <lukeab@gmail.com>
2017-11-19 17:27:03 -0800

    cleanup

--- keras-cnn/cnn.py
+++ keras-cnn/cnn.py
@@ -31,11 +31,6 @@ model.add(Conv2D(32,
     input_shape=(28, 28,1),
     activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32,
-    (config.first_layer_conv_width, config.first_layer_conv_height),
-    activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Flatten())
 model.add(Dense(config.dense_layer_size, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
