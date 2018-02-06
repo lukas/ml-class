2c7db4c44ec7e930f237cefc35c2271b684739d0
Lukas Biewald <lukeab@gmail.com>
2018-02-05 20:47:28 -0800

    Removed batches

--- keras-mlp/mlp.py
+++ keras-mlp/mlp.py
@@ -35,7 +35,6 @@ model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=config.optimizer,
                     metrics=['accuracy'])
 
-
 # Fit the model
 model.fit(X_train, y_train, validation_data=(X_test, y_test),
         callbacks=[WandbKerasCallback()], epochs=config.epochs)
