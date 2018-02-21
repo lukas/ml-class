4892ca0a3df99b6bc321d500375143c0da3e17e1
Lukas Biewald <lukas@Lukass-MBP.localdomain>
2018-02-20 16:52:30 -0800

    new class

---
+++ keras-fashion/perceptron-linear.py
@@ -0,0 +1,56 @@
+import numpy
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
+from keras.utils import np_utils
+import wandb
+from wandb.wandb_keras import WandbKerasCallback
+
+# logging code
+run = wandb.init()
+config = run.config
+config.epochs = 10
+
+# load data
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+img_width = X_train.shape[1]
+img_height = X_train.shape[2]
+
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_train.shape[1]
+
+# create model
+model=Sequential()
+model.add(Flatten(input_shape=(img_width,img_height)))
+model.add(Dense(num_classes))
+model.compile(loss='mse', optimizer='adam',
+                metrics=['accuracy'])
+
+# Fit the model
+model.fit(X_train, y_train, epochs=config.epochs, validation_data=(X_test, y_test),
+                    callbacks=[WandbKerasCallback()])
+
+
+# Output some predictions
+
+from PIL import Image
+from PIL import ImageDraw 
+import numpy as np
+
+model_output = model.predict(X_test)
+
+labels =["T-shirt/top","Trouser","Pullover","Dress",
+    "Coat","Sandal","Shirt","Sneaker","Bag","Ankle boot"]
+
+for i in range(10):
+    prediction = np.argmax(model_output[i])
+    img = Image.fromarray(X_test[i])
+    img = img.resize((280, 280), Image.ANTIALIAS)
+
+    draw = ImageDraw.Draw(img)
+    draw.text((10, 10),labels[prediction],(255))
+    img.save(str(i)+".jpg")
