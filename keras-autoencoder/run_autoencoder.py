c176c701c7c29a2ac6bfa9d33230d9c814a2f2c6
Lukas Biewald <lukeb@cs.stanford.edu>
2018-04-02 08:44:37 -0700

    Cleaned up autoencoder

--- keras-autoencoder/run_autoencoder.py
+++ keras-autoencoder/run_autoencoder.py
@@ -1,4 +1,3 @@
-
 from keras.models import Model
 from keras.models import load_model
 
@@ -8,17 +7,32 @@ import cv2
 
 (x_train, _), (x_test, _) = mnist.load_data()
 
-model = load_model('auto.h5')
-output = model.predict(x_test[:1])
+model = load_model('auto-denoise.h5')
 
-cv2.imshow('output.png', output[0].reshape(28,28,1))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-def add_noise(x_train, x_test):
+def add_noise(x_train):
     noise_factor = 0.5
     x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
-    x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
-    
+        
     x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-    x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+    return x_train_noisyx
+
+i = 0
+while(True):
+
+  k = cv2.waitKey(0)
+  if k == 27:         # wait for ESC key to exit                                                                                                             cv2.destroyAllWindows()
+    break
+
+  input_img = x_test[i]
+
+  if k == 32:   # space bar
+      input_img = add_noise(input_img)
+
+  output_img = model.predict(input_img.reshape(1,28,28))[0].reshape(28,28,1)
+  cv2.imshow('input', input_img)
+  cv2.imshow('output', output_img) 
+  i+=1
+ 
+
+
+
