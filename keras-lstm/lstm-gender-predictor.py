d9695ae769c56fe4977a3b62aba7c191b9b9996e
l2k2 <l2k2@local>
2018-07-30 23:44:12 +0000

    Gender predictor

--- keras-lstm/lstm-gender-predictor.py
+++ keras-lstm/lstm-gender-predictor.py
@@ -1,37 +1,49 @@
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
-from keras import __version__ as keras_version
+import wandb
+from wandb.keras import WandbCallback
+
 import numpy as np
 
+wandb.init()
+config = wandb.config
 
-nEpochs = 10
-weightsFileName = "gender_weights.h5"
-with open("male.txt") as f:
-    m_names = f.readlines()
+config.epochs = 10
+config.batch_size = 16
 
-with open("female.txt") as f:
-    f_names = f.readlines()
+def load_names():
+    with open("male.txt") as f:
+        m_names = f.readlines()
 
-mf_names = []
+    with open("female.txt") as f:
+        f_names = f.readlines()
 
-for f_name in f_names:
-	if f_name in m_names:
-		mf_names.append(f_name)
+    mf_names = []
 
-m_names = [m_name.lower() for m_name in m_names if not m_name in mf_names]
-f_names = [f_name.lower() for f_name in f_names if not f_name in mf_names]
+    # remove the names that are both male and female
+    for f_name in f_names:
+        if f_name in m_names:
+            mf_names.append(f_name)
 
+    m_names = [m_name.lower() for m_name in m_names if not m_name in mf_names]
+    f_names = [f_name.lower() for f_name in f_names if not f_name in mf_names]
 
+    return m_names, f_names
+
+m_names, f_names = load_names()
+    
 totalEntries = len(m_names) + len(f_names)
-maxlen = len(max( m_names , key=len)) + len(max( f_names , key=len))
+maxlen = 20
 
 chars = set(  "".join(m_names) + "".join(f_names)  )
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
-X = np.zeros((totalEntries , maxlen, len(chars) ), dtype=np.bool)
-y = np.zeros((totalEntries , 2 ), dtype=np.bool)
+X = np.zeros((totalEntries , maxlen, len(chars) ), dtype=np.float32)
+y = np.zeros((totalEntries , 2 ), dtype=np.float32)
+
+print(m_names)
 
 for i, name in enumerate(m_names):
     for t, char in enumerate(name):
@@ -49,28 +61,17 @@ def vec2c(vec):
 			return indices_char[i]
 	return ""
 
+print(X)
+print(y)
+
 model = Sequential()
 model.add(LSTM(512, return_sequences=True, input_shape=(maxlen, len(chars))))
 model.add(Dropout(0.2))
 model.add(LSTM(512, return_sequences=False))
 model.add(Dropout(0.2))
-model.add(Dense(2))
-model.add(Activation('softmax'))
+model.add(Dense(2, activation='softmax'))
 
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-json_string = model.to_json()
+model.fit(X, y,validation_split=0.2, callbacks=[WandbCallback()])
 
-with open("model.json", "w") as text_file:
-    text_file.write(json_string)
-
-
-if keras_version[0] == '1':
-	model.fit(X, y, batch_size=16, nb_epoch=nEpochs)
-else:
-	model.fit(X, y, batch_size=16, epochs=nEpochs,validation_split=0.2)
-
-model.save('gender.h5')
-
-score = model.evaluate(X, y, batch_size=16)
-print "score " , score
