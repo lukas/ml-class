3dc09927c1f7ce7b8519d1d57a4388e49832cd9d
l2k2 <l2k2@local>
2018-08-20 22:51:42 +0000

    cleanup

--- videos/lstm-classifier/imdb-lstm.py
+++ videos/lstm-classifier/imdb-lstm.py
@@ -37,11 +37,6 @@ model = Sequential()
 model.add(Embedding(config.vocab_size,
                     config.embedding_dims,
                     input_length=config.maxlen))
-model.add(Conv1D(config.filters,
-                 config.kernel_size,
-                 padding='valid',
-                 activation='relu'))
-model.add(MaxPooling1D())
 model.add(LSTM(config.hidden_dims, activation="sigmoid"))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy',
