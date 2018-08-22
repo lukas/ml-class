2a0d715aaf794fce4edafe2d854337568058aec1
vanpelt <vanpelt@local>
2018-08-22 07:01:36 +0000

    Fixed autoencoders

--- keras-autoencoder/autoencoder_cnn.py
+++ keras-autoencoder/autoencoder_cnn.py
@@ -2,21 +2,31 @@ from keras.layers import Input, Dense, Flatten, Reshape, Conv2D, UpSampling2D, M
 from keras.models import Model, Sequential
 from keras.datasets import mnist
 from keras.callbacks import Callback
-from autoencoder import Images
 
 import numpy as np
+from util import Images
 import wandb
 from wandb.keras import WandbCallback
+class Images(Callback):
+    def on_epoch_end(self, epoch, logs):
+        indices = np.random.randint(self.validation_data[0].shape[0], size=8)
+        test_data = self.validation_data[0][indices]
+        pred_data = self.model.predict(test_data)
+        wandb.log({
+             "examples": [
+                   wandb.Image(np.hstack([data, pred_data[i]]), caption=str(i))
+                   for i, data in enumerate(test_data)]
+        }, commit=False)
 
 run = wandb.init()
 config = run.config
 
 config.epochs = 10
 
-(x_train, _), (x_test, _) = mnist.load_data()
+(X_train, _), (X_test, _) = mnist.load_data()
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
+X_train = X_train.astype('float32') / 255.
+X_test = X_test.astype('float32') / 255.
 
 model = Sequential()
 model.add(Reshape((28,28,1), input_shape=(28,28)))
@@ -29,12 +39,10 @@ model.add(Reshape((28,28)))
 
 model.compile(optimizer='adam', loss='mse')
 
-model.fit(x_train, x_train,
-                epochs=config.epochs,
-                validation_data=(x_test, x_test), 
-                callbacks=[Images(), WandbCallback(save_model=False)])
+model.fit(X_train, X_train,
+          epochs=config.epochs,
+          validation_data=(X_test, X_test), 
+          callbacks=[Images(), WandbCallback(save_model=False)])
 
 
 model.save('auto-cnn.h5')
-
-
