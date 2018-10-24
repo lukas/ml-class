4506ca66129c2dcef2993a59558c066785d2379d
Chris Van Pelt <vanpelt@gmail.com>
2018-10-23 21:23:26 -0700

    Updates for class

--- keras-perceptron/perceptron-linear.py
+++ keras-perceptron/perceptron-linear.py
@@ -28,15 +28,14 @@ labels = range(10)
 num_classes = y_train.shape[1]
 
 # create model
-model=Sequential()
-model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(num_classes, activation="softmax"))
+model = Sequential()
+model.add(Flatten(input_shape=(img_width, img_height)))
+model.add(Dense(num_classes))
 model.compile(loss='categorical_crossentropy', optimizer='adam',
-                metrics=['accuracy'])
+              metrics=['accuracy'])
 
 # Fit the model
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test),
-                    callbacks=[WandbCallback(data_type="image", labels=labels, save_model=False)])
+          callbacks=[WandbCallback(data_type="image", labels=labels, save_model=False)])
 print(model.predict(X_test[:10]))
 model.save('model.h5')
-
