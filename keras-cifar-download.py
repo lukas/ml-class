daab40f643a8d90332aec2a0febbbf57c90cc159
Lukas Biewald <lukeb@cs.stanford.edu>
2018-11-13 15:29:28 -0800

    Delete keras-cifar-download.py

--- keras-cifar-download.py
+++
@@ -1,9 +0,0 @@
-from keras.datasets import cifar10
-from keras.applications.inception_v3 import InceptionV3
-from keras.applications.resnet50 import ResNet50
-
-model = ResNet50(weights='imagenet')
-model = InceptionV3(weights='imagenet')
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-print("Success!")
