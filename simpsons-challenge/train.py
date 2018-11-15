e964c0b5193e51f86a1d033ddfd3d3ee119fe752
Chris Van Pelt <vanpelt@gmail.com>
2018-11-15 06:10:57 -0800

    New projects for London class

--- simpsons-challenge/train.py
+++ simpsons-challenge/train.py
@@ -21,7 +21,8 @@ if config.team is None:
 # download the data if it doesn't exist
 if not os.path.exists("simpsons"):
     print("Downloading Simpsons dataset...")
-    subprocess.check_output("curl https://storage.googleapis.com/wandb-production.appspot.com/mlclass/simpsons.tar.gz | tar xvz", shell=True)
+    subprocess.check_output(
+        "curl https://storage.googleapis.com/wandb-production.appspot.com/mlclass/simpsons.tar.gz | tar xvz", shell=True)
 
 # this is the augmentation configuration we will use for training
 # see: https://keras.io/preprocessing/image/#imagedatagenerator-class
@@ -36,19 +37,20 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 # batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
     'simpsons/train',  # this is the target directory
-    target_size=(config.img_size,config.img_size),
+    target_size=(config.img_size, config.img_size),
     batch_size=config.batch_size)
 
 # this is a similar generator, for validation data
 test_generator = test_datagen.flow_from_directory(
-        'simpsons/test',
-        target_size=(config.img_size,config.img_size),
-        batch_size=config.batch_size)
+    'simpsons/test',
+    target_size=(config.img_size, config.img_size),
+    batch_size=config.batch_size)
 
 labels = list(test_generator.class_indices.keys())
 
 model = Sequential()
-model.add(Conv2D(16, (3,3), input_shape=(config.img_size, config.img_size, 3), activation="relu"))
+model.add(Conv2D(16, (3, 3), input_shape=(
+    config.img_size, config.img_size, 3), activation="relu"))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dropout(0.4))
@@ -57,10 +59,11 @@ model.compile(optimizer=optimizers.Adam(),
               loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.fit_generator(
-        train_generator,
-        steps_per_epoch=len(train_generator),
-        epochs=config.epochs,
-        workers=4,
-        validation_data=test_generator,
-        callbacks=[WandbCallback(data_type="image", labels=labels, generator=test_generator, save_model=False)],
-        validation_steps=len(test_generator))
+    train_generator,
+    steps_per_epoch=len(train_generator),
+    epochs=config.epochs,
+    workers=4,
+    validation_data=test_generator,
+    callbacks=[WandbCallback(
+        data_type="image", labels=labels, generator=test_generator, save_model=False)],
+    validation_steps=len(test_generator))
