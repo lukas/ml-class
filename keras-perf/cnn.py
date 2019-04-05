9d6d59565bfbf63204c9ea3960328117ec80e85f
Chris Van Pelt <vanpelt@gmail.com>
2019-04-05 10:31:59 -0400

    Fix tflite conversion

--- keras-perf/cnn.py
+++ keras-perf/cnn.py
@@ -1,3 +1,4 @@
+from keras import backend as K
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
@@ -55,20 +56,46 @@ model.compile(loss='categorical_crossentropy', optimizer='adam',
 
 model.fit(X_train, y_train, validation_data=(X_test, y_test),
           epochs=config.epochs,
-          callbacks=[WandbCallback(data_type="image", save_model=False),    
+          callbacks=[WandbCallback(data_type="image", save_model=False),
                      TensorBoard(log_dir=wandb.run.dir)])
 
-from keras import backend as K
-import tensorflow as tf
-from tensorflow.python.tools import freeze_graph
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(
+            set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            session, input_graph_def, output_names, freeze_var_names)
+        return frozen_graph
 
 
 model.save("cnn.h5")
 saver = tf.train.Saver()
-saver.save(K.get_session(), './keras_model.ckpt')
+sess = K.get_session()
+saver.save(sess, './keras_model.ckpt')
+tf.train.write_graph(sess.graph_def, '.', 'keras_model.pbtxt')
 
-freeze_graph.freeze_graph('tensorflowModel.pbtxt', "", False, 
-                          './tensorflowModel.ckpt', "output/softmax",
-                           "save/restore_all", "save/Const:0",
-                           'frozentensorflowModel.pb', True, ""  
-                         )
+frozen_graph = freeze_session(K.get_session(), output_names=[
+                              out.op.name for out in model.outputs])
+tf.train.write_graph(frozen_graph, ".", "cnn.pb", as_text=False)
