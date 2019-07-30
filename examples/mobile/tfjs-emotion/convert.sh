9e9158db1aacd8d96b0192e0a6a3c0d1c1893fde
Chris Van Pelt <vanpelt@gmail.com>
2019-07-29 21:41:53 -0700

    Cleanup perceptron dir

--- examples/mobile/tfjs-emotion/convert.sh
+++ examples/mobile/tfjs-emotion/convert.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+virtualenv --no-site-packages venv
+. venv/bin/activate
+pip install tensorflowjs
 
 tensorflowjs_converter --input_format keras --quantization_bytes 2 emotion.h5 app/models
 
+deactivate
 echo "Model converted, run ./serve.sh"
