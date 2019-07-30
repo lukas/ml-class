992ff05abc5ae7c1aa82c835d382dbe344bb7b68
Chris Van Pelt <vanpelt@gmail.com>
2019-07-30 14:08:58 -0700

    Bring back mobilenet

---
+++ examples/keras-transfer/mobilenet-inspect.py
@@ -0,0 +1,20 @@
+from keras.applications.mobilenet_v2 import MobileNetV2
+from keras.preprocessing import image
+from keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+import numpy as np
+import time
+
+model = MobileNetV2(weights='imagenet')
+
+img_path = 'elephant.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+model.summary()
+t = time.time()
+preds = model.predict(x)
+
+print('Predicted:', decode_predictions(preds, top=3)[0])
+print('Took: %.2f seconds' % (time.time() - t))
+# model.save('image.h5')
