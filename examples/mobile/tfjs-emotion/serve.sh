e64b9f67395d905db2321b88cec53c713aebc033
Chris Van Pelt <vanpelt@gmail.com>
2019-07-31 20:45:52 -0700

    Class updates

--- examples/mobile/tfjs-emotion/serve.sh
+++ examples/mobile/tfjs-emotion/serve.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-cd app && python -m http.server &
-ssh -R 80:localhost:8000 serveo.net
+{ cd app && python -m http.server; } &
+{ cd ssh -R 80:localhost:8000 serveo.net; } &
+wait -n
+pkill -P $$
