971abcc4662ba67a57883bc440ea70828234626d
Chris Van Pelt <vanpelt@gmail.com>
2019-09-10 23:02:33 -0500

    Fix embeddings

--- examples/lstm/imdb-classifier/util.py
+++ examples/lstm/imdb-classifier/util.py
@@ -9,38 +9,33 @@ def load_imdb():
         subprocess.check_output(
             "curl -SL http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz | tar xz", shell=True)
 
-    if not os.path.exists('./aclImdb/cache.npz'):
-        X_train = []
-        y_train = []
+    X_train = []
+    y_train = []
 
-        path = './aclImdb/train/pos/'
-        X_train.extend([open(path + f).read()
-                        for f in os.listdir(path) if f.endswith('.txt')])
-        y_train.extend([1 for _ in range(12500)])
+    path = './aclImdb/train/pos/'
+    X_train.extend([open(path + f).read()
+                    for f in os.listdir(path) if f.endswith('.txt')])
+    y_train.extend([1 for _ in range(12500)])
 
-        path = './aclImdb/train/neg/'
-        X_train.extend([open(path + f).read()
-                        for f in os.listdir(path) if f.endswith('.txt')])
-        y_train.extend([0 for _ in range(12500)])
+    path = './aclImdb/train/neg/'
+    X_train.extend([open(path + f).read()
+                    for f in os.listdir(path) if f.endswith('.txt')])
+    y_train.extend([0 for _ in range(12500)])
 
-        X_test = []
-        y_test = []
+    X_test = []
+    y_test = []
 
-        path = './aclImdb/test/pos/'
-        X_test.extend([open(path + f).read()
-                       for f in os.listdir(path) if f.endswith('.txt')])
-        y_test.extend([1 for _ in range(12500)])
+    path = './aclImdb/test/pos/'
+    X_test.extend([open(path + f).read()
+                   for f in os.listdir(path) if f.endswith('.txt')])
+    y_test.extend([1 for _ in range(12500)])
 
-        path = './aclImdb/test/neg/'
-        X_test.extend([open(path + f).read()
-                       for f in os.listdir(path) if f.endswith('.txt')])
-        y_test.extend([0 for _ in range(12500)])
+    path = './aclImdb/test/neg/'
+    X_test.extend([open(path + f).read()
+                   for f in os.listdir(path) if f.endswith('.txt')])
+    y_test.extend([0 for _ in range(12500)])
 
-        np.savez('./aclImdb/cache.npz', X_train=X_train, y_train=y_train,
-                 X_test=X_test, y_test=y_test)
-
-    cached = np.load('./aclImdb/cache.npz')
-    return (cached['X_train'], cached['y_train']), (cached['X_test'], cached['y_test'])
+    return (X_train, y_train), (X_test, y_test)
 
 
 if __name__ == '__main__':
