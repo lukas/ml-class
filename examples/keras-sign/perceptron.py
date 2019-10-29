afce7fb31aebb3091e72e1b5e9d78df7045cb885
Lukas Biewald <lukas@wandb.com>
2019-10-29 15:49:53 +0000

    fix perceptron

--- examples/keras-sign/perceptron.py
+++ examples/keras-sign/perceptron.py
@@ -26,24 +26,9 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_train.shape[1]
 
-# you may want to normalize the data here..
-
-# normalize data
-X_train = X_train.astype('float32') / 255.
-X_test = X_test.astype('float32') / 255.
-
 # create model
 model = Sequential()
-model.add(Reshape((img_width, img_height, 1), input_shape=(img_width, img_height)))
-model.add(Conv2D(32, (3,3), activation="relu"))
-model.add(MaxPooling2D())
-model.add(Dropout(0.25))
-model.add(Conv2D(64, (3,3), activation="relu"))
-model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dropout(0.25))
-model.add(Dense(100, activation="relu"))
-model.add(Dropout(0.25))
 model.add(Dense(num_classes, activation="softmax"))
 model.compile(loss=config.loss, optimizer=config.optimizer,
               metrics=['accuracy'])
