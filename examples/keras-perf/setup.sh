fe9eac4440ec51f575ec4d1318719da5a14adfd3
Chris Van Pelt <vanpelt@gmail.com>
2019-12-05 23:58:35 -0800

    Use 1.14 for profiling

--- examples/keras-perf/setup.sh
+++ examples/keras-perf/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 virtualenv --no-site-packages venv
 . venv/bin/activate
-pip install tf-nightly-2.0-preview wandb pillow ipykernel
-python -m ipykernel install --user --name=tf2.0
+pip install tensorflow_gpu==1.14.0 wandb pillow ipykernel
+python -m ipykernel install --user --name=tf1.14
