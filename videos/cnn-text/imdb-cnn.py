0e05bdff9737906f99ff7979a19e5a7c3f869fab
Chris Van Pelt <vanpelt@gmail.com>
2020-01-11 11:09:27 -0800

    Use the built-in keras dataset

--- videos/cnn-text/imdb-cnn.py
+++ videos/cnn-text/imdb-cnn.py
@@ -6,7 +6,6 @@ from keras.layers import Conv1D, Flatten, MaxPooling1D
 from keras.datasets import imdb
 import wandb
 from wandb.keras import WandbCallback
-import imdb
 import numpy as np
 from keras.preprocessing import text
 
@@ -23,12 +22,7 @@ config.kernel_size = 3
 config.hidden_dims = 250
 config.epochs = 10
 
-(X_train, y_train), (X_test, y_test) = imdb.load_imdb()
-
-tokenizer = text.Tokenizer(num_words=config.vocab_size)
-tokenizer.fit_on_texts(X_train)
-X_train = tokenizer.texts_to_matrix(X_train)
-X_test = tokenizer.texts_to_matrix(X_test)
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=config.vocab_size, maxlen=config.maxlen)
 
 X_train = sequence.pad_sequences(X_train, maxlen=config.maxlen)
 X_test = sequence.pad_sequences(X_test, maxlen=config.maxlen)
