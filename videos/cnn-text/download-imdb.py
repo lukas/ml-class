b118c1a3f7713d997d197b38de6b8bd7dec528e8
charlesfrye <charles@wandb.com>
2020-09-04 14:07:43 -0700

    reimplements downloader for imdb

--- videos/cnn-text/download-imdb.py
+++ videos/cnn-text/download-imdb.py
@@ -1,26 +1,29 @@
 import os
+import shutil
+import sys
+import tempfile
+import urllib.request
 
-def load_imdb():
-    X_train = []
-    y_train = []
 
-    path = './aclImdb/train/pos/'
-    X_train.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
-    y_train.extend([1 for _ in range(12500)])
+IMDB_URL = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+OUTPUT_NAME = "aclImdb"
 
-    path = './aclImdb/train/neg/'
-    X_train.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
-    y_train.extend([0 for _ in range(12500)])
+def main():
+    download_and_extract_archive()
 
-    X_test = []
-    y_test = []
-    
-    path = './aclImdb/test/pos/'
-    X_test.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
-    y_test.extend([1 for _ in range(12500)])
 
-    path = './aclImdb/test/neg/'
-    X_test.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
-    y_test.extend([0 for _ in range(12500)])
-    
-    return (X_train, y_train), (X_test, y_test)
+def download_and_extract_archive():
+    if os.path.exists(OUTPUT_NAME):
+        print("Imdb dataset download target exists at " + OUTPUT_NAME)
+    else:
+        with urllib.request.urlopen(IMDB_URL) as response:
+            with tempfile.NamedTemporaryFile() as temp_archive:
+                temp_archive.write(response.read())
+                imdb_tar = shutil.unpack_archive(
+                    temp_archive.name, extract_dir=".", format="gztar")
+
+    return
+
+
+if __name__ == "__main__":
+    sys.exit(main())
