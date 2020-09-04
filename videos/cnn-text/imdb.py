858b9fc0507ca3823ed0d4b0d2a7d75e5765f15e
charlesfrye <charles@wandb.com>
2020-09-04 14:08:13 -0700

    generalizes to work on windows, fixes typecasting error

--- videos/cnn-text/imdb.py
+++ videos/cnn-text/imdb.py
@@ -1,26 +1,32 @@
+import numpy as np
 import os
+
+sep = os.path.sep
 
 def load_imdb():
     X_train = []
     y_train = []
 
-    path = './aclImdb/train/pos/'
+    path = os.path.join('aclImdb', 'train', 'pos', '')
     X_train.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
     y_train.extend([1 for _ in range(12500)])
 
-    path = './aclImdb/train/neg/'
+    path = os.path.join('aclImdb', 'train', 'neg', '')
     X_train.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
     y_train.extend([0 for _ in range(12500)])
 
     X_test = []
     y_test = []
-    
-    path = './aclImdb/test/pos/'
+
+    path = os.path.join('aclImdb', 'test', 'pos', '')
     X_test.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
     y_test.extend([1 for _ in range(12500)])
 
-    path = './aclImdb/test/neg/'
+    path = os.path.join('aclImdb', 'test', 'neg', '')
     X_test.extend([open(path + f).read() for f in os.listdir(path) if f.endswith('.txt')])
     y_test.extend([0 for _ in range(12500)])
-    
+
+    y_train = np.array(y_train, dtype=np.int32)
+    y_test = np.array(y_test, dtype=np.int32)
+
     return (X_train, y_train), (X_test, y_test)
