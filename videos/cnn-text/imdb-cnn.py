2b794a2572925e652a36b9bc5b767239331b1f10
charlesfrye <charles@wandb.com>
2020-09-04 14:08:53 -0700

    reverts to version used in video

--- videos/cnn-text/imdb-cnn.py
+++ videos/cnn-text/imdb-cnn.py
@@ -9,6 +9,8 @@ from wandb.keras import WandbCallback
 import numpy as np
 from keras.preprocessing import text
 
+import imdb
+
 wandb.init()
 config = wandb.config
 
@@ -22,7 +24,13 @@ config.kernel_size = 3
 config.hidden_dims = 250
 config.epochs = 10
 
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=config.vocab_size, maxlen=config.maxlen)
+(X_train, y_train), (X_test, y_test) = imdb.load_imdb()
+
+
+tokenizer = text.Tokenizer(num_words=config.vocab_size)
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_matrix(X_train)
+X_test = tokenizer.texts_to_matrix(X_test)
 
 X_train = sequence.pad_sequences(X_train, maxlen=config.maxlen)
 X_test = sequence.pad_sequences(X_test, maxlen=config.maxlen)
